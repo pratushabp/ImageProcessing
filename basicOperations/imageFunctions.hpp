29b96e44ef7360ca65a2cb8a8142c402b3ebb986
pratushabp <34252569+pratushabp@users.noreply.github.com>
2017-12-05 16:08:12 -0800

    added function headers

--- basicOperations/imageFunctions.hpp
+++ basicOperations/imageFunctions.hpp
@@ -1,1 +1,28 @@
+#include <vector>
+#include <fstream>
+#include <new>
+#include <iterator>
+#include <algorithm>
+#include <math.h>
+#include <iterator>
+#include <algorithm>
+#include <math.h>
+#include <map>
+#include <random>
+#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
 
+void crop(int, char* )
+void resize(int, char* )
+void cmyk(int, char* )
+void hsl(int, char* )
+void histEqualisationGrayBucket(int, char* )
+void histEqualisationGrayTF(int, char* )
+void histEqualisationColorBucket(int, char* )
+void histEqualisationColorTF(int, char* )
+void specialEffect(int, char* )
+void histTransform(int, char* )
+void mixNoise(int, char* )
+void nonLocalMean(int, char* )
